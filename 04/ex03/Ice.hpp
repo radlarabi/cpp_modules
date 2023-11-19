33584929490e70e5c80d351d43d4d50a5f8cd988
Radouan Larabi <rlarabi@e1r3p8.1337.ma>
2023-11-19 20:43:41 +0100

    ex03 04

--- 04/ex03/Ice.hpp
+++ 04/ex03/Ice.hpp
@@ -1,12 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Ice.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rlarabi <rlarabi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/19 20:34:19 by rlarabi           #+#    #+#             */
-/*   Updated: 2023/11/19 20:34:32 by rlarabi          ###   ########.fr       */
+/*   Created: 2023/11/19 20:40:10 by rlarabi           #+#    #+#             */
+/*   Updated: 2023/11/19 20:42:51 by rlarabi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ICE_HPP
+#define ICE_HPP
+#include "AMateria.hpp"
+
+class Ice : public Amateria
+{
+public:
+    Ice();
+    Ice(Ice const &ice);
+    Ice &operator=(Ice const &icr);
+    ~Ice();
+};
+
+#endif
