71b215c4a087ac72f7ac9b60a60fc3de40d1eeb3
Radouan Larabi <larabi.radouan@gmail.com>
2023-11-24 15:02:50 +0100

    change array to struct

--- 04/ex03/Struct.hpp
+++ 04/ex03/Struct.hpp
@@ -6,14 +6,14 @@
 /*   By: rlarabi <rlarabi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 13:42:25 by rlarabi           #+#    #+#             */
-/*   Updated: 2023/11/24 13:45:48 by rlarabi          ###   ########.fr       */
+/*   Updated: 2023/11/24 15:02:43 by rlarabi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCT_HPP
 #define STRUCT_HPP
 #include "AMateria.hpp"
-
+ 
 typedef struct Sadrs{
     AMateria *materia;
     struct Sadrs *next;
