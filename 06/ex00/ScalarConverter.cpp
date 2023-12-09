72e96cfefb0c0ae17552a622a0d8129009e8e2c2
Radouan Larabi <larabi.radouan@gmail.com>
2023-12-09 15:10:55 +0100

    ex00

--- 06/ex00/ScalarConverter.cpp
+++ 06/ex00/ScalarConverter.cpp
@@ -6,23 +6,85 @@
 /*   By: rlarabi <rlarabi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 23:48:28 by rlarabi           #+#    #+#             */
-/*   Updated: 2023/12/08 00:14:16 by rlarabi          ###   ########.fr       */
+/*   Updated: 2023/12/09 15:09:32 by rlarabi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
-ScalarConverter::ScalarConverter(){   
+ScalarConverter::ScalarConverter(){}
+
+
+ScalarConverter::ScalarConverter(ScalarConverter const &other){
+    (void)other;   
 }
 
-void ScalarConverter::convert(std::string _type)
+ScalarConverter &ScalarConverter::operator = (ScalarConverter const &other){
+    (void)other;
+    return (*this);
+}
+
+ScalarConverter::~ScalarConverter(){}
+
+void toChar(double value, char *endptr)
 {
-    try {
-        int intValue = std::atoi(_type.c_str());
-        std::cout << "Int : " << intValue << std::endl;
-    } catch (const std::invalid_argument& e) {
-        std::cerr << "Int : Impossible\n";
-    } catch (const std::out_of_range& e) {
-        std::cerr << "Int : Impossible\n";
+    if (!strcmp(endptr, "nan")|| strlen(endptr) != 0 || value > INT_MAX || value < INT_MIN)
+        throw std::runtime_error("Char : Impossible");
+    if (value < 33 || value > 126)
+        throw std::runtime_error("Char : Non displayable");
+    std::cout << "Char : " <<  static_cast<char>(value) << std::endl;
+}
+
+void toInt(double value, char *endptr)
+{
+    if (strlen(endptr) != 0 || value > INT_MAX || value < INT_MIN)
+        throw std::runtime_error("Int : Impossible");
+    std::cout << "Int : " <<  static_cast<int>(value) << std::endl;
+}
+
+void toFloat(double value, char *endptr)
+{
+    if (strlen(endptr) != 0 || value > FLT_MAX || value < -FLT_MIN)
+        throw std::runtime_error("Float : Impossible");
+    std::cout << "Float : " << std::fixed << std::setprecision(1) << static_cast<float>(value) << "f" << std::endl;
+}
+
+void toDouble(double value, char *endptr)
+{
+    if (strlen(endptr) != 0|| value > DBL_MAX || value < -DBL_MIN)
+        throw std::runtime_error("Double : Impossible");
+    std::cout << "Double : " << std::fixed << std::setprecision(1) << value << std::endl;
+}
+
+void ScalarConverter::convert(std::string _type){
+    char* endptr = NULL;
+    double value = strtod(_type.c_str(), &endptr);
+    try{
+        toChar(value, endptr);
     }
+    catch(const std::exception& e){
+        std::cerr << e.what() << '\n';
+    }
+    
+    try{
+        toInt(value, endptr);
+    }
+    catch(const std::exception& e){
+        std::cerr << e.what() << '\n';
+    }
+    
+    try{
+        toFloat(value, endptr);
+    }
+    catch(const std::exception& e){
+        std::cerr << e.what() << '\n';
+    }
+    
+    try{
+        toDouble(value, endptr);
+    }
+    catch(const std::exception& e){
+        std::cerr << e.what() << '\n';
+    }
+    
 }
--- 06/ex00/ScalarConverter.hpp
+++ 06/ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: rlarabi <rlarabi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 23:41:33 by rlarabi           #+#    #+#             */
-/*   Updated: 2023/12/08 00:12:08 by rlarabi          ###   ########.fr       */
+/*   Updated: 2023/12/09 14:25:26 by rlarabi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,17 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
-
+#include <iomanip>
+#include <cstring>
+#include <climits>
+#include <cfloat>
 class ScalarConverter{
-
-public:
+private:
     ScalarConverter();
+    ~ScalarConverter();
+public:
     ScalarConverter(ScalarConverter const &other);
     ScalarConverter &operator = (ScalarConverter const &other);
-    ~ScalarConverter();
     static void convert(std::string type);
 };
 
