6df6b52ac855a7cd66a34807680e1637a3851819
Radouan Larabi <larabi.radouan@gmail.com>
2023-12-09 20:19:36 +0100

    ex01

--- 06/ex00/ScalarConverter.cpp
+++ 06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: rlarabi <rlarabi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 23:48:28 by rlarabi           #+#    #+#             */
-/*   Updated: 2023/12/09 15:09:32 by rlarabi          ###   ########.fr       */
+/*   Updated: 2023/12/09 18:09:03 by rlarabi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ ScalarConverter::~ScalarConverter(){}
 
 void toChar(double value, char *endptr)
 {
-    if (!strcmp(endptr, "nan")|| strlen(endptr) != 0 || value > INT_MAX || value < INT_MIN)
+    if (!strcmp(endptr, "nan")|| (strlen(endptr) >= 1 && strcmp(endptr, "f")) || value > INT_MAX || value < INT_MIN)
         throw std::runtime_error("Char : Impossible");
     if (value < 33 || value > 126)
         throw std::runtime_error("Char : Non displayable");
@@ -37,21 +37,21 @@ void toChar(double value, char *endptr)
 
 void toInt(double value, char *endptr)
 {
-    if (strlen(endptr) != 0 || value > INT_MAX || value < INT_MIN)
+    if ((strlen(endptr) >= 1 && strcmp(endptr, "f")) || value > INT_MAX || value < INT_MIN)
         throw std::runtime_error("Int : Impossible");
     std::cout << "Int : " <<  static_cast<int>(value) << std::endl;
 }
 
 void toFloat(double value, char *endptr)
 {
-    if (strlen(endptr) != 0 || value > FLT_MAX || value < -FLT_MIN)
+    if ((strlen(endptr) >= 1 && strcmp(endptr, "f")) || value > FLT_MAX || value < -FLT_MIN)
         throw std::runtime_error("Float : Impossible");
     std::cout << "Float : " << std::fixed << std::setprecision(1) << static_cast<float>(value) << "f" << std::endl;
 }
 
 void toDouble(double value, char *endptr)
 {
-    if (strlen(endptr) != 0|| value > DBL_MAX || value < -DBL_MIN)
+    if ((strlen(endptr) >= 1 && strcmp(endptr, "f")) || value > DBL_MAX || value < -DBL_MIN)
         throw std::runtime_error("Double : Impossible");
     std::cout << "Double : " << std::fixed << std::setprecision(1) << value << std::endl;
 }
@@ -59,6 +59,7 @@ void toDouble(double value, char *endptr)
 void ScalarConverter::convert(std::string _type){
     char* endptr = NULL;
     double value = strtod(_type.c_str(), &endptr);
+    
     try{
         toChar(value, endptr);
     }
