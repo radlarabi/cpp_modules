4767b6c31aba48db70fe2d730210f4fbc9b6fb9c
Radouan Larabi <larabi.radouan@gmail.com>
2023-12-11 14:28:24 +0100

    init ex02

---
+++ 06/ex02/Base.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Base.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlarabi <rlarabi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/11 14:23:34 by rlarabi           #+#    #+#             */
+/*   Updated: 2023/12/11 14:26:06 by rlarabi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BASE_HPP
+#define BASE_HPP
+#include <iostream>
+
+class Base
+{
+public:
+    virtual ~Base();
+};
+
+
+class A : public Base
+{
+public:
+    A();
+    ~A();
+};
+
+class B : public Base
+{
+public:
+    B();
+    ~B();
+};
+
+class C : public Base
+{
+public:
+    C();
+    ~C();
+};
+Base * generate(void);
+void identify(Base* p);
+void identify(Base& p);
+
+#endif
